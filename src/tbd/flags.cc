60d93f0bdc53d26092cbc2a063369b79327e4956
inoahdev <inoahdev@gmail.com>
2017-07-16 22:09:07 +0530

    Fix spaces in flags.cc

--- src/tbd/flags.cc
+++ src/tbd/flags.cc
@@ -39,29 +39,29 @@ void flags::cast(long index, bool result) noexcept {
 
         const auto bit_size = this->bit_size();
         const auto bits_length = bit_size * length_;
-        
+
         // As length_ can be of any size, it is not possible to
         // simply cast flags_.ptr to unsigned int *, instead, the
         // pointer must be (temporarily) advanced to allow casting
         // as unsigned int *.
-        
+
         // To accomplish this, the provided index must be inversed.
         // The index is from "front" (from the right if visualised
         // as in a hex editor), the pointer however points to the back
         // of its bytes. As such it becomes necessary to calculate the
         // index from the back.
-        
+
         auto index_from_back = (bits_length - 1) - index;
-        
+
         // As length_ is larger than sizeof(unsigned int)
         // It is often necessary to advance the flags-pointer
         // to the next byte to access the bit at the index the
         // caller provided.
-        
+
         // To advance the flags-pointer to the right byte(s), the
         // flags-pointer is advanced by one byte until index_from_back
         // is smaller than bit_size (bit-count of unsigned int).
-        
+
         if (index_from_back > bit_size) {
             do {
                 index_from_back -= 8;
@@ -91,39 +91,39 @@ void flags::cast(long index, bool result) noexcept {
 bool flags::at_index(long index) const noexcept {
     if (length_ > bit_size()) {
         auto ptr = (unsigned int *)flags_.ptr;
-        
+
         const auto bit_size = this->bit_size();
         const auto bits_length = bit_size * length_;
-        
+
         // As length_ can be of any size, it is not possible to
         // simply cast flags_.ptr to unsigned int *, instead, the
         // pointer must be (temporarily) advanced to allow casting
         // as unsigned int *.
-        
+
         // To accomplish this, the provided index must be inversed.
         // The index is from "front" (from the right if visualised
         // as in a hex editor), the pointer however points to the back
         // of its bytes. As such it becomes necessary to calculate the
         // index from the back.
-        
+
         auto index_from_back = (bits_length - 1) - index;
-        
+
         // As length_ is larger than sizeof(unsigned int)
         // It is often necessary to advance the flags-pointer
         // to the next byte to access the bit at the index the
         // caller provided.
-        
+
         // To advance the flags-pointer to the right byte(s), the
         // flags-pointer is advanced by one byte until index_from_back
         // is smaller than bit_size (bit-count of unsigned int).
-        
+
         if (index_from_back > bit_size) {
             do {
                 index_from_back -= 8;
                 ptr = (unsigned int *)((uintptr_t)ptr + 1);
             } while (index_from_back > bit_size);
         }
-        
+
         return (*ptr & 1 << index) ? true : false;
     } else {
         const auto flags = flags_.flags;
