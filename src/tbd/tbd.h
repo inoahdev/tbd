212dad4bf311924a5f9df144a326338e3b341fe8
inoahdev <inoahdev@gmail.com>
2017-08-11 10:49:15 -0700

    Revert tbd-class into a namespace

--- src/tbd/tbd.h
+++ src/tbd/tbd.h
@@ -13,8 +13,7 @@
 
 #include "symbol.h"
 
-class tbd {
-public:
+namespace tbd {
     enum platform {
         ios = 1,
         macosx,
@@ -22,50 +21,14 @@ public:
         tvos
     };
 
-    static const char *platform_to_string(const platform &platform) noexcept;
-    static platform string_to_platform(const char *platform) noexcept;
+    __attribute__((unused)) static const char *platform_to_string(const platform &platform) noexcept;
+    __attribute__((unused)) static platform string_to_platform(const char *platform) noexcept;
 
     enum class version {
         v1 = 1,
         v2
     };
 
-    static version string_to_version(const char *version) noexcept;
-
-    explicit tbd() = default;
-    explicit tbd(const std::vector<std::string> &macho_files, const std::vector<std::string> &output_files, const platform &platform, const version &version, const std::vector<const macho::architecture_info *> &architecture_overrides = std::vector<const macho::architecture_info *>());
-
-    void run();
-
-    inline std::vector<std::string> &macho_files() noexcept { return macho_files_; }
-    inline std::vector<std::string> &output_files() noexcept { return output_files_; }
-
-    inline const std::vector<const macho::architecture_info *> architectures() const noexcept { return architectures_; }
-
-    inline void set_architectures(const std::vector<const macho::architecture_info *> &architectures) noexcept {
-        architectures_ = architectures;
-    }
-
-    inline void set_platform(const platform &platform) noexcept {
-        platform_ = platform;
-    }
-
-    inline void set_version(const version &version) noexcept {
-        version_ = version;
-    }
-
-    inline const platform &platform() const noexcept { return platform_; }
-    inline const version &version() const noexcept { return version_; }
-
-private:
-    std::vector<std::string> macho_files_;
-    std::vector<std::string> output_files_;
-
-    enum platform platform_;
-    enum version version_;
-
-    std::vector<const macho::architecture_info *> architectures_;
-
-    void print_symbols(FILE *output_file, const flags &flags, std::vector<symbol> &symbols, enum symbol::type type) const noexcept;
-    void run(macho::file &macho, FILE *output);
-};
+    __attribute__((unused)) static version string_to_version(const char *version) noexcept;
+    __attribute__((unused)) void create_from_macho_library(macho::file &library, FILE *output, unsigned int options, std::vector<macho::architecture_info> &architecture_overrides);
+}
