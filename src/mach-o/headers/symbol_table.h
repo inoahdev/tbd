86ef4a0babf88b307e232ef5deddd0973cd7949d
inoahdev <inoahdev@gmail.com>
2017-09-17 14:54:25 -0700

    Add operators for macho::symbol_table::description

--- src/mach-o/headers/symbol_table.h
+++ src/mach-o/headers/symbol_table.h
@@ -51,7 +51,22 @@ namespace macho {
             indirect = 0xa
         };
 
-        enum description : uint16_t {
+        inline bool operator==(const flags &flags, const uint8_t &number) noexcept { return (uint8_t)flags == number; }
+        inline bool operator==(const uint8_t &number, const flags &flags) noexcept { return flags == (uint8_t)number; }
+
+        inline bool operator!=(const flags &flags, const uint8_t &number) noexcept { return (uint8_t)flags != number; }
+        inline bool operator!=(const uint8_t &number, const flags &flags) noexcept { return number != (uint8_t)flags; }
+
+        inline bool operator==(const type &type, const uint8_t &number) noexcept { return (uint8_t)type == number; }
+        inline bool operator==(const uint8_t &number, type type) noexcept { return type == (uint8_t)number; }
+
+        inline bool operator!=(const type &type, const uint8_t &number) noexcept { return (uint8_t)type != number; }
+        inline bool operator!=(const uint8_t &number, type type) noexcept { return number != (uint8_t)type; }
+
+        inline int operator&(const uint8_t &number, const flags &flags) noexcept { return number & (uint8_t)flags; }
+        inline int operator&(const flags &flags, const uint8_t &number) noexcept { return (uint8_t)flags & number; }
+
+        enum class description : int16_t {
             no_dead_strip = 0x0020,
             weak_reference = 0x0040,
             weak_definition = 0x0080,
@@ -60,19 +75,34 @@ namespace macho {
             alternate_entry = 0x0200
         };
 
-        inline bool operator==(flags flags, int number) noexcept { return (int)flags == number; }
-        inline bool operator==(int number, flags flags) noexcept { return flags == (int)number; }
+        inline int16_t operator|(const int16_t &lhs, const description &rhs) noexcept { return lhs | (int16_t)rhs; }
+        inline void operator|=(int16_t &lhs, const description &rhs) noexcept { lhs |= (int16_t)rhs; }
 
-        inline bool operator!=(flags flags, int number) noexcept { return (int)flags != number; }
-        inline bool operator!=(int number, flags flags) noexcept { return number != (int)flags; }
+        inline description operator|(const description &lhs, const int16_t &rhs) noexcept { return (description)((int16_t)lhs | rhs); }
+        inline void operator|=(description &lhs, const int16_t &rhs) noexcept { lhs = (description)((int16_t)lhs | rhs); }
 
-        inline bool operator==(type type, int number) noexcept { return (int)type == number; }
-        inline bool operator==(int number, type type) noexcept { return type == (int)number; }
+        inline description operator|(const description &lhs, const description &rhs) noexcept { return (description)((int16_t)lhs | (int16_t)rhs); }
+        inline void operator|=(description &lhs, const description &rhs) noexcept { lhs = (description)((int16_t)lhs | (int16_t)rhs); }
 
-        inline bool operator!=(type type, int number) noexcept { return (int)type != number; }
-        inline bool operator!=(int number, type type) noexcept { return number != (int)type; }
+        inline int16_t operator&(const int16_t &lhs, const description &rhs) noexcept { return lhs & (int16_t)rhs; }
+        inline void operator&=(int16_t &lhs, const description &rhs) noexcept { lhs &= (int16_t)rhs; }
 
-        inline int operator&(int number, flags flags) noexcept { return number & (int)flags; }
-        inline int operator&(flags flags, int number) noexcept { return (int)flags & number; }
+        inline description operator&(const description &lhs, const int16_t &rhs) noexcept { return (description)((int16_t)lhs & rhs); }
+        inline void operator&=(description &lhs, const int16_t &rhs) noexcept { lhs = (description)((int16_t)lhs & rhs); }
+
+        inline description operator&(const description &lhs, const description &rhs) noexcept { return (description)((int16_t)lhs & (int16_t)rhs); }
+        inline void operator&=(description &lhs, const description &rhs) noexcept { lhs = (description)((int16_t)lhs & (int16_t)rhs); }
+
+        inline uint16_t operator|(const uint16_t &lhs, const description &rhs) noexcept { return lhs | (uint16_t)rhs; }
+        inline void operator|=(uint16_t &lhs, const description &rhs) noexcept { lhs |= (uint16_t)rhs; }
+
+        inline description operator|(const description &lhs, const uint16_t &rhs) noexcept { return (description)((uint16_t)lhs | rhs); }
+        inline void operator|=(description &lhs, const uint16_t &rhs) noexcept { lhs = (description)((uint16_t)lhs | rhs); }
+
+        inline int16_t operator&(const uint16_t &lhs, const description &rhs) noexcept { return lhs & (uint16_t)rhs; }
+        inline void operator&=(uint16_t &lhs, const description &rhs) noexcept { lhs &= (uint16_t)rhs; }
+
+        inline description operator&(const description &lhs, const uint16_t &rhs) noexcept { return (description)((uint16_t)lhs & rhs); }
+        inline void operator&=(description &lhs, const uint16_t &rhs) noexcept { lhs = (description)((uint16_t)lhs & rhs); }
     }
 }
