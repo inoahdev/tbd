8030bcd6b7bc76a092c7c53c0c17ec142b8fa6d7
inoahdev <inoahdev@gmail.com>
2018-01-25 15:58:35 -0800

    Add documentation of the inner workings of recursive-remove

--- src/recursive/remove.cc
+++ src/recursive/remove.cc
@@ -18,13 +18,28 @@ namespace recursive::remove {
     }
 
     result perform(char *path, char *begin) {
+        // To perform a recursive-remove, we start from
+        // the last path-component and iterate backwards
+        // until we reach a path-component beginning at
+        // caller argument "begin"
+
         auto end = utils::string::find_end_of_null_terminated_string(path);
 
-        auto prev_path_component_end = begin;
+        // Use a "prev_path_component_end" variable to store
+        // the previous path-component end. This is needed
+        // because path-component end is allowed to hit begin
+        // but not be below it, so we use "prev_path_component_end"
+        // to check if we have already been at begin and are
+        // now, at least supposed to be, lower
+
         auto path_component_end = end;
+        auto prev_path_component_end = path_component_end;
 
         do {
-            auto path_component_end_elmt = *path_component_end;
+            // Terminate path at path-component end to get
+            // full path of the current path-component
+
+            auto path_component_end_chr = *path_component_end;
             *path_component_end = '\0';
 
             if (access(path, F_OK) != 0) {
@@ -43,10 +58,10 @@ namespace recursive::remove {
                 return result::failed_to_remove_subdirectories;
             }
 
-            *path_component_end = path_component_end_elmt;
+            *path_component_end = path_component_end_chr;
 
             prev_path_component_end = path_component_end;
-            path_component_end = utils::path::find_last_slash_in_front_of_pattern(begin, prev_path_component_end);
+            path_component_end = utils::path::find_last_slash_in_front_of_pattern(begin, path_component_end);
         } while (prev_path_component_end != begin);
 
         return result::ok;
