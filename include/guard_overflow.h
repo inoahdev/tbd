da6d42acfb7f6b65bbc95e52e01147d2335f490f
inoahdev <inoahdev@gmail.com>
2019-02-13 16:38:19 -0800

    Fix compile errors with different integer-types

--- include/guard_overflow.h
+++ include/guard_overflow.h
@@ -11,15 +11,11 @@
 
 #include <stdint.h>
 
-#define guard_overflow_add(left_in, right) _Generic((left_in), \
-    uint32_t *: __builtin_uadd_overflow, \
-    uint64_t *: __builtin_uaddl_overflow \
-) (*left_in, right, left_in)
+#define guard_overflow_add(left_in, right) \
+    __builtin_add_overflow(*left_in, right, left_in)
 
-#define guard_overflow_mul(left_in, right) _Generic((left_in), \
-    uint32_t *: __builtin_umul_overflow, \
-    uint64_t *: __builtin_umull_overflow \
-) (*left_in, right, left_in)
+#define guard_overflow_mul(left_in, right) \
+    __builtin_mul_overflow(*left_in, right, left_in)
 
 #define guard_overflow_shift_left(left_in, right) _Generic((left_in), \
     uint32_t *: guard_overflow_shift_left_uint32, \
