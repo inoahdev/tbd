8304fbe7642b87c084268c32737d24db60610529
inoahdev <inoahdev@gmail.com>
2019-11-11 20:42:26 -0500

    Replace dynamic impl of arch_info func with static impl

--- src/arch_info.c
+++ src/arch_info.c
@@ -11,8 +11,6 @@
 #include <string.h>
 
 #include "mach/machine.h"
-
-#include "array.h"
 #include "arch_info.h"
 
 /*
@@ -162,54 +160,6 @@ static struct arch_info arch_info_list[] = {
     { 0, 0, NULL, 0 }
 };
 
-/*
- * Create a fake array to pass binary-search onto array's functions.
- */
-
-static const struct array arch_info_array = {
-    .data = (void *)arch_info_list,
-    .data_end = (void *)(arch_info_list + 56),
-    .alloc_end = (void *)(arch_info_list + 56)
-};
-
-struct arch_info_cputype_info {
-    cpu_type_t cputype;
-
-    uint64_t front;
-    uint64_t back;
-};
-
-/*
- * To support the use of fake-arrays, we don't const our cputype-info table.
- */
-
-static struct arch_info_cputype_info cputype_info_list[] = {
-    { CPU_TYPE_ANY,        0, 2  },
-    { CPU_TYPE_MC680x0,    3, 5  },
-    { CPU_TYPE_X86,        6, 14 },
-    { CPU_TYPE_HPPA,      15, 16 },
-    { CPU_TYPE_ARM,       17, 29 },
-    { CPU_TYPE_MC88000,   30, 30 },
-    { CPU_TYPE_SPARC,     31, 31 },
-    { CPU_TYPE_I860,      32, 32 },
-    { CPU_TYPE_POWERPC,   33, 44 },
-    { CPU_TYPE_VEO,       45, 47 },
-    { CPU_TYPE_X86_64,    48, 50 },
-    { CPU_TYPE_ARM64,     51, 53 },
-    { CPU_TYPE_POWERPC64, 54, 55 },
-    { CPU_TYPE_ARM64_32,  56, 56 }
-};
-
-/*
- * Create a fake array to pass binary-search onto array's functions.
- */
-
-const struct array cputype_info_array = {
-    .data = (void *)cputype_info_list,
-    .data_end = (void *)(cputype_info_list + 14),
-    .alloc_end = (void *)(cputype_info_list + 14)
-};
-
 const struct arch_info *__notnull arch_info_get_list(void) {
     return arch_info_list;
 }
@@ -218,101 +168,289 @@ uint64_t arch_info_list_get_size(void) {
     return sizeof(arch_info_list) / sizeof(struct arch_info);
 }
 
-static uint64_t cputype_info_list_get_size(void) {
-    return sizeof(cputype_info_list) / sizeof(struct arch_info_cputype_info);
-}
-
-static int
-cputype_info_comparator(const void *__notnull const left,
-                        const void *__notnull const right)
-{
-    const struct arch_info *const info = (const struct arch_info *)left;
-
-    const cpu_type_t table_cputype = info->cputype;
-    const cpu_type_t cputype = *(const cpu_type_t *)right;
-
-    if (table_cputype > cputype) {
-        return 1;
-    } else if (table_cputype < cputype) {
-        return -1;
-    }
-
-    return 0;
-}
-
-static int
-arch_info_cpusubtype_comparator(const void *__notnull const left,
-                                const void *__notnull const right)
-{
-    const struct arch_info *const info = (const struct arch_info *)left;
-
-    const cpu_subtype_t table_cpusubtype = info->cpusubtype;
-    const cpu_subtype_t cpusubtype = *(const cpu_subtype_t *)right;
-
-    if (table_cpusubtype > cpusubtype) {
-        return 1;
-    } else if (table_cpusubtype < cpusubtype) {
-        return -1;
-    }
-
-    return 0;
-}
-
 const struct arch_info *
 arch_info_for_cputype(const cpu_type_t cputype, const cpu_subtype_t cpusubtype)
 {
-    /*
-     * First find the cputype-info for the cputype provided to get the range
-     * inside arch_info_list, where we will then search within for the right
-     * arch.
-     */
+    switch (cputype) {
+        case CPU_TYPE_ANY:
+            switch (cpusubtype) {
+                case CPU_SUBTYPE_MULTIPLE:
+                    return (arch_info_list + 0);
 
-    const struct array_slice cputype_info_slice = {
-        .front = 0,
-        .back = cputype_info_list_get_size() - 1
-    };
+                case CPU_SUBTYPE_LITTLE_ENDIAN:
+                    return (arch_info_list + 1);
 
-    const struct arch_info_cputype_info *const info =
-        array_find_item_in_sorted_with_slice(
-            &cputype_info_array,
-            sizeof(struct arch_info_cputype_info),
-            cputype_info_slice,
-            &cputype,
-            cputype_info_comparator,
-            NULL);
+                case CPU_SUBTYPE_BIG_ENDIAN:
+                    return (arch_info_list + 2);
 
-    if (info == NULL) {
-        return NULL;
+                default:
+                    return NULL;
+            }
+
+            break;
+
+        case CPU_TYPE_MC680x0:
+            switch (cpusubtype) {
+                case CPU_SUBTYPE_MC680x0_ALL:
+                    return (arch_info_list + 3);
+
+                case CPU_SUBTYPE_MC68040:
+                    return (arch_info_list + 4);
+
+                case CPU_SUBTYPE_MC68030_ONLY:
+                    return (arch_info_list + 5);
+
+                default:
+                    return NULL;
+            }
+
+            break;
+
+        case CPU_TYPE_X86:
+            switch (cpusubtype) {
+                case CPU_SUBTYPE_I386_ALL:
+                    return (arch_info_list + 6);
+
+                case CPU_SUBTYPE_486:
+                    return (arch_info_list + 7);
+
+                case CPU_SUBTYPE_486SX:
+                    return (arch_info_list + 8);
+
+                case CPU_SUBTYPE_PENT:
+                    return (arch_info_list + 9);
+
+                case CPU_SUBTYPE_PENTPRO:
+                    return (arch_info_list + 10);
+
+                case CPU_SUBTYPE_PENTII_M3:
+                    return (arch_info_list + 11);
+
+                case CPU_SUBTYPE_PENTII_M5:
+                    return (arch_info_list + 12);
+
+                case CPU_SUBTYPE_PENTIUM_4:
+                    return (arch_info_list + 13);
+
+                case CPU_SUBTYPE_X86_64_H:
+                    return (arch_info_list + 14);
+
+                default:
+                    return NULL;
+            }
+
+            break;
+
+        case CPU_TYPE_HPPA:
+            /*
+             * CPU_SUBTYPE_HPPA_ALL and CPU_SUBTYPE_HPPA_7100 are the same
+             * value.
+             */
+
+            if (cpusubtype == CPU_SUBTYPE_HPPA_ALL) {
+                return (arch_info_list + 15);
+            }
+
+            return NULL;
+
+        case CPU_TYPE_ARM:
+            switch (cpusubtype) {
+                /*
+                 * Skip one index because of CPU_SUBTYPE_HPPA_7100 situation
+                 * mentioned above.
+                 */
+
+                case CPU_SUBTYPE_ARM_ALL:
+                    return (arch_info_list + 17);
+
+                case CPU_SUBTYPE_ARM_V4T:
+                    return (arch_info_list + 18);
+
+                case CPU_SUBTYPE_ARM_V6:
+                    return (arch_info_list + 19);
+
+                case CPU_SUBTYPE_ARM_V5TEJ:
+                    return (arch_info_list + 20);
+
+                case CPU_SUBTYPE_ARM_XSCALE:
+                    return (arch_info_list + 21);
+
+                case CPU_SUBTYPE_ARM_V7:
+                    return (arch_info_list + 22);
+
+                case CPU_SUBTYPE_ARM_V7F:
+                    return (arch_info_list + 23);
+
+                case CPU_SUBTYPE_ARM_V7S:
+                    return (arch_info_list + 24);
+
+                case CPU_SUBTYPE_ARM_V7K:
+                    return (arch_info_list + 25);
+
+                case CPU_SUBTYPE_ARM_V6M:
+                    return (arch_info_list + 26);
+
+                case CPU_SUBTYPE_ARM_V7M:
+                    return (arch_info_list + 27);
+
+                case CPU_SUBTYPE_ARM_V7EM:
+                    return (arch_info_list + 28);
+
+                case CPU_SUBTYPE_ARM_V8:
+                    return (arch_info_list + 29);
+
+                default:
+                    return NULL;
+            }
+
+            break;
+
+        case CPU_TYPE_MC88000:
+            if (cpusubtype == CPU_SUBTYPE_MC88000_ALL) {
+                return (arch_info_list + 30);
+            }
+
+            return NULL;
+
+        case CPU_TYPE_SPARC:
+            if (cpusubtype == CPU_SUBTYPE_SPARC_ALL) {
+                return (arch_info_list + 31);
+            }
+
+            return NULL;
+
+        case CPU_TYPE_I860:
+            if (cpusubtype == CPU_SUBTYPE_I860_ALL) {
+                return (arch_info_list + 32);
+            }
+
+            return NULL;
+
+        case CPU_TYPE_POWERPC:
+            switch (cpusubtype) {
+                case CPU_SUBTYPE_POWERPC_ALL:
+                    return (arch_info_list + 33);
+
+                case CPU_SUBTYPE_POWERPC_601:
+                    return (arch_info_list + 34);
+
+                case CPU_SUBTYPE_POWERPC_602:
+                    return (arch_info_list + 35);
+
+                case CPU_SUBTYPE_POWERPC_603:
+                    return (arch_info_list + 36);
+
+                case CPU_SUBTYPE_POWERPC_603e:
+                    return (arch_info_list + 37);
+
+                case CPU_SUBTYPE_POWERPC_603ev:
+                    return (arch_info_list + 38);
+
+                case CPU_SUBTYPE_POWERPC_604:
+                    return (arch_info_list + 39);
+
+                case CPU_SUBTYPE_POWERPC_604e:
+                    return (arch_info_list + 40);
+
+                case CPU_SUBTYPE_POWERPC_750:
+                    return (arch_info_list + 41);
+
+                case CPU_SUBTYPE_POWERPC_7400:
+                    return (arch_info_list + 42);
+
+                case CPU_SUBTYPE_POWERPC_7450:
+                    return (arch_info_list + 43);
+
+                case CPU_SUBTYPE_POWERPC_970:
+                    return (arch_info_list + 44);
+
+                default:
+                    return NULL;
+            }
+
+            break;
+
+        case CPU_TYPE_VEO:
+            switch (cpusubtype) {
+                /*
+                 * CPU_SUBTYPE_VEO_ALL is defined as CPU_SUBTYPE_VEO_2
+                 */
+
+                case CPU_SUBTYPE_VEO_ALL:
+                    return (arch_info_list + 45);
+
+                case CPU_SUBTYPE_VEO_1:
+                    return (arch_info_list + 46);
+
+                default:
+                    return NULL;
+            }
+
+            break;
+
+        case CPU_TYPE_X86_64:
+            switch (cpusubtype) {
+                /*
+                 * Skip one index because of the CPU_SUBTYPE_VEO_2 situation
+                 * described above.
+                 */
+
+                case CPU_SUBTYPE_X86_64_ALL_LIB64:
+                    return (arch_info_list + 48);
+
+                case CPU_SUBTYPE_X86_64_ALL:
+                    return (arch_info_list + 49);
+
+                case CPU_SUBTYPE_X86_64_H:
+                    return (arch_info_list + 50);
+
+                default:
+                    return NULL;
+            }
+
+            break;
+
+        case CPU_TYPE_ARM64:
+            switch (cpusubtype) {
+                case CPU_SUBTYPE_ARM64_ALL:
+                    return (arch_info_list + 51);
+
+                case CPU_SUBTYPE_ARM64_V8:
+                    return (arch_info_list + 52);
+
+                case CPU_SUBTYPE_ARM64E:
+                    return (arch_info_list + 53);
+
+                default:
+                    return NULL;
+            }
+
+            break;
+
+        case CPU_TYPE_POWERPC64:
+            switch (cpusubtype) {
+                case CPU_SUBTYPE_POWERPC_ALL:
+                    return (arch_info_list + 54);
+
+                case CPU_SUBTYPE_POWERPC_970:
+                    return (arch_info_list + 55);
+
+                default:
+                    return NULL;
+            }
+
+            break;
+
+        case CPU_TYPE_ARM64_32:
+            if (cpusubtype == CPU_SUBTYPE_ARM64_ALL) {
+                return (arch_info_list + 56);
+            }
+
+            return NULL;
+
+        default:
+            return NULL;
     }
-
-    const uint64_t info_front_index = info->front;
-    if (info_front_index == info->back) {
-        const struct arch_info *const arch = arch_info_list + info_front_index;
-        if (arch->cpusubtype == cpusubtype) {
-            return arch;
-        }
-
-        return NULL;
-    }
-
-    const struct array_slice slice = {
-        .front = info_front_index,
-        .back = info->back
-    };
-
-    const struct arch_info *const arch =
-        array_find_item_in_sorted_with_slice(&arch_info_array,
-                                             sizeof(struct arch_info),
-                                             slice,
-                                             &cpusubtype,
-                                             arch_info_cpusubtype_comparator,
-                                             NULL);
-
-    if (arch == NULL) {
-        return NULL;
-    }
-
-    return arch;
 }
 
 const struct arch_info *arch_info_for_name(const char *__notnull const name) {
